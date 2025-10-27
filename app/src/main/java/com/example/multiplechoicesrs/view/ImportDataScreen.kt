e0b98f987928b0938f8c976cfd7c53c794bacb32
Dorothee Erps <240172@jc-21.jp>
2025-10-27 12:47:54 +0900

    Generate Testdata

--- app/src/main/java/com/example/multiplechoicesrs/view/ImportDataScreen.kt
+++ app/src/main/java/com/example/multiplechoicesrs/view/ImportDataScreen.kt
@@ -1,19 +1,25 @@
 package com.example.multiplechoicesrs.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import com.example.multiplechoicesrs.R
+import com.example.multiplechoicesrs.db.DeckTableHelper
+import com.example.multiplechoicesrs.model.Deck
 
 @Composable
 fun ImportDataScreen(
     navBack: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val deckTableHelper = DeckTableHelper(LocalContext.current)
+
     ProvideAppBarTitle {
         Text("インポート")
     }
@@ -32,6 +38,22 @@ fun ImportDataScreen(
     }
 
     Column(modifier = modifier) {
-        Text("Load data")
+        Text("Generate Testdata")
+
+        Button(onClick = {
+            deckTableHelper.saveDeck(Deck(
+                1,
+                "応用情報技術者試験",
+                1
+            ))
+
+            deckTableHelper.saveDeck(Deck(
+                2,
+                "日本語能力試験",
+                1
+            ))
+        }) {
+            Text("Import")
+        }
     }
 }
