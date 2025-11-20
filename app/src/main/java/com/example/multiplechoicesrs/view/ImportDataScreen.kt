5267f2c42fe7055e73379a56886ebf34f64add15
Dorothee Erps <240172@jc-21.jp>
2025-11-20 16:07:21 +0900

    Added Snackbar to inform about success/failure of import

--- app/src/main/java/com/example/multiplechoicesrs/view/ImportDataScreen.kt
+++ app/src/main/java/com/example/multiplechoicesrs/view/ImportDataScreen.kt
@@ -1,5 +1,6 @@
 package com.example.multiplechoicesrs.view
 
+import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
@@ -21,11 +22,20 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -40,6 +50,8 @@ import com.example.multiplechoicesrs.model.DecksJson
 import com.example.multiplechoicesrs.rest.ImportDecksUiState
 import com.example.multiplechoicesrs.rest.ImportDecksViewModel
 import com.example.multiplechoicesrs.rest.MultipleChoiceApi
+import com.example.multiplechoicesrs.ui.theme.GreenCorrectAnswer
+import com.example.multiplechoicesrs.ui.theme.RedIncorrectAnswer
 import com.example.multiplechoicesrs.view.custom.ProvideAppBarNavigationIcon
 import com.example.multiplechoicesrs.view.custom.ProvideAppBarTitle
 import kotlinx.coroutines.launch
@@ -88,68 +100,98 @@ fun ImportDecksScreen(
     }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun ImportDecksListScreen(decks: DecksJson, modifier: Modifier = Modifier) {
     val deckTableHelper = DeckTableHelper(LocalContext.current)
     val categoryTableHelper = CategoryTableHelper(LocalContext.current)
     val questionTableHelper = QuestionTableHelper(LocalContext.current)
     val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
 
-    LazyColumn(
-        modifier = modifier.padding(10.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+    var importStatus by remember { mutableStateOf(ImportStatus.UNKNOWN) }
+
+    Scaffold(
+        snackbarHost = {
+            SnackbarHost(snackbarHostState) { data ->
+                Snackbar(
+                    containerColor = getSnackbarColor(importStatus),
+                    snackbarData = data
+                )
+            }
+        }
     ) {
-        items(decks.data) { deck ->
-            Card(
-                elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = MaterialTheme.colorScheme.background,
-                ),
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.fillMaxWidth().padding(10.dp)
+        LazyColumn(
+            modifier = modifier.padding(10.dp),
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            items(decks.data) { deck ->
+                Card(
+                    elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.background,
+                    ),
                 ) {
-                    Text(
-                        deck.name,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.onBackground
-                    )
-
-                    Button(onClick = {
-                        //TODO: Check ob import notwendig (version_id)
-                        //Falls nicht: dialog
-                        coroutineScope.launch {
-                            try {
-                                val result = MultipleChoiceApi.retrofitService.importDeck(deck.deckId)
-
-                                deckTableHelper.saveDeck(result)
-                                result.categories?.forEach {
-                                    categoryTableHelper.saveCategory(it)
-                                }
-                                result.questionsJson?.forEach {
-                                    questionTableHelper.saveQuestion(it)
-                                }
-
-                                //TODO: Erfolgsmeldung
-                            } catch (e: Exception) {
-                                Log.d("TEST", e.toString())
-                                //TODO: Fehlermeldung
-                            }
-                        }
-                    }) {
-                        Icon(
-                            painter = painterResource(R.drawable.baseline_download_24),
-                            contentDescription = "インポート"
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth().padding(10.dp)
+                    ) {
+                        Text(
+                            deck.name,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.onBackground
                         )
+
+                        Button(onClick = {
+                            //TODO: Check ob import notwendig (version_id)
+                            //Falls nicht: dialog
+                            coroutineScope.launch {
+                                try {
+                                    val result = MultipleChoiceApi.retrofitService.importDeck(deck.deckId)
+
+                                    deckTableHelper.saveDeck(result)
+                                    result.categories?.forEach {
+                                        categoryTableHelper.saveCategory(it)
+                                    }
+                                    result.questionsJson?.forEach {
+                                        questionTableHelper.saveQuestion(it)
+                                    }
+
+                                    importStatus = ImportStatus.SUCCESS
+                                    snackbarHostState.showSnackbar("デッキがインポートされました")
+                                } catch (e: Exception) {
+                                    Log.d("ERROR", e.toString())
+                                    importStatus = ImportStatus.FAILURE
+                                    snackbarHostState.showSnackbar("障害が発生しました")
+                                }
+                            }
+                        }) {
+                            Icon(
+                                painter = painterResource(R.drawable.baseline_download_24),
+                                contentDescription = "インポート"
+                            )
+                        }
                     }
                 }
             }
         }
     }
+}
 
+enum class ImportStatus {
+    SUCCESS,
+    FAILURE,
+    UNKNOWN
+}
+
+fun getSnackbarColor(importStatus: ImportStatus): Color {
+    return when(importStatus) {
+        ImportStatus.SUCCESS -> GreenCorrectAnswer
+        ImportStatus.FAILURE -> RedIncorrectAnswer
+        ImportStatus.UNKNOWN -> Color.Gray
+    }
 }
 
 @Composable
