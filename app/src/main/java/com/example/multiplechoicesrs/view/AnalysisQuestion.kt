34ae62a33598002b994ac6147e62c4b5cf5f8d5b
Dorothee Erps <240172@jc-21.jp>
2025-11-21 12:23:50 +0900

    Filter question list via filtertext

--- app/src/main/java/com/example/multiplechoicesrs/view/AnalysisQuestion.kt
+++ app/src/main/java/com/example/multiplechoicesrs/view/AnalysisQuestion.kt
@@ -1,14 +1,18 @@
 package com.example.multiplechoicesrs.view
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,11 +22,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.multiplechoicesrs.R
 import com.example.multiplechoicesrs.model.Deck
 import com.example.multiplechoicesrs.model.Question
-import com.example.multiplechoicesrs.model.viewmodel.AnalysisDeckUiState
 import com.example.multiplechoicesrs.model.viewmodel.AnalysisQuestionData
 import com.example.multiplechoicesrs.model.viewmodel.AnalysisQuestionUiState
 import com.example.multiplechoicesrs.model.viewmodel.AnalysisQuestionViewModel
@@ -55,6 +60,8 @@ fun AnalysisQuestionRouting(
 
 @Composable
 fun AnalysisQuestionList(list: List<AnalysisQuestionData>) {
+    var filterText by remember { mutableStateOf("") }
+
     var selectedQuestion: Question? by remember { mutableStateOf(null) }
     var showQuestionDialog by remember { mutableStateOf(false) }
 
@@ -64,12 +71,49 @@ fun AnalysisQuestionList(list: List<AnalysisQuestionData>) {
         }
     }
 
-    Column {
-        //TODO: Filtering
+    Column(
+        verticalArrangement = Arrangement.spacedBy(15.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            OutlinedTextField(
+                value = filterText,
+                onValueChange = { filterText = it },
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(R.drawable.baseline_search_24),
+                        contentDescription = "フィルター"
+                    )
+                },
+                modifier = Modifier.weight(1f)
+            )
+
+            OutlinedButton(
+                onClick = {
+                    //TODO: Filter categories
+                },
+                modifier = Modifier.size(46.dp),
+                contentPadding = PaddingValues(0.dp),
+                shape = RoundedCornerShape(15)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.baseline_filter_alt_24),
+                    contentDescription = "分野フィルター",
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .size(36.dp)
+                )
+            }
+        }
+
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(10.dp)
         ) {
-            items(list) { item ->
+            items(list.filter { item ->
+                item.question.source.contains(filterText)
+            }) { item ->
                 AnalysisQuestionItem(item) {
                     selectedQuestion = item.question
                     showQuestionDialog = true
