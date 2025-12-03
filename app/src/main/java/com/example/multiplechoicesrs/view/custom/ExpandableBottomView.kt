d1e5372c3807bb9a452fe58f07b7121e6c09ce3e
Dorothee Erps <240172@jc-21.jp>
2025-12-03 14:30:02 +0900

    Background of ExpandableBottomView not transparent

--- app/src/main/java/com/example/multiplechoicesrs/view/custom/ExpandableBottomView.kt
+++ app/src/main/java/com/example/multiplechoicesrs/view/custom/ExpandableBottomView.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -40,6 +41,7 @@ fun ExpandableBottomView(
 
     Box(
         Modifier
+            .background(MaterialTheme.colorScheme.background)
             .zIndex(2f)
     ) {
         Column {
